80ad5fd9631f0e6189bd6b07a846546f1445d621
Anastasiia Kosterkina <nastya.kosterkina@mail.ru>
2023-12-26 21:28:24 +0300

    testng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,36 +1,55 @@
 package ru.netology.service;
 
-import static org.junit.Assert.assertEquals;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
 
 
-class CashbackHackServiceTest {
-    @org.junit.Test
-    public void shouldReturn200at800() {
+    @Test
+    public void shouldReturn100at900() {
         CashbackHackService service = new CashbackHackService();
-        int expected = 200;
-        int actual = service.remain(800);
+        int expected = 100;
+        int actual = service.remain(900);
 
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
-    @org.junit.Test
+    @Test
     public void shouldReturn1at999() {
         CashbackHackService service = new CashbackHackService();
         int expected = 1;
         int actual = service.remain(999);
-        assertEquals(expected, actual);
 
-
+        assertEquals(actual, expected);
     }
 
-    @org.junit.Test
+    @Test
+    public void shouldReturn999at1() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 999;
+        int actual = service.remain(1);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
     public void shouldReturn0at1000() {
         CashbackHackService service = new CashbackHackService();
         int expected = 0;
         int actual = service.remain(1000);
 
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
+    @Test
+    public void shouldReturn999at1001() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 999;
+        int actual = service.remain(1001);
+
+        assertEquals(actual, expected);
+    }
 
 }
